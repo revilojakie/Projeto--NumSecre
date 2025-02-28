99b096a8de4cb57e1a80739355256eada24988b8
Oliver <olivervictorrad@gmail.com>
2025-02-28 20:10:32 -0300

    Vontando a Inico do projeto

--- desafio 08/logica-js-projeto_inicial/app.js
+++
@@ -1,8 +0,0 @@
- function olaMundo() {
-  console.log('Olá, mundo!');
- }
-
- function olaPessoa(nome) {
-    input = document.getElementById('nome').textContent;
-    console.log('Olá, ' + nome + '!');
- }
