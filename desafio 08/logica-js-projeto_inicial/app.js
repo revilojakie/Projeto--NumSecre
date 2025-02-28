1cec8e11dc2061e9081aa1e29a40fadc5f86cb1a
Oliver <olivervictorrad@gmail.com>
2025-02-28 19:42:37 -0300

    enviando versão 1

---
+++ desafio 08/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+ function olaMundo() {
+  console.log('Olá, mundo!');
+ }
+
+ function olaPessoa(nome) {
+    input = document.getElementById('nome').textContent;
+    console.log('Olá, ' + nome + '!');
+ }
---
+++ desafio 09/logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+
+let nome = prompt ('Digite seu nome')
+let idade = prompt ('Digite sua idade')
+if (idade >= 18){
+    alert ('VocVoce pode tirar sua habilitação')
+}
