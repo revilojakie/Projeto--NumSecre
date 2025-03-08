5be0029c2ae288d3aa18df47a8149c4d5c160f88
Oliver <olivervictorrad@gmail.com>
2025-03-08 11:37:31 -0300

    fase 1 Codigo completo

--- challenge-amigo-secreto/challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/challenge-amigo-secreto/app.js
@@ -1,1 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+    
+    listaAmigos.push(nome);
+    input.value = "";
+    atualizarLista();
+}
+console.log(listaAmigos);
+function atualizarLista() {
+    const ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    listaAmigos.forEach(nome => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        ul.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("Adicione pelo menos um nome antes de sortear!");
+        return;
+    }
+    
+    const indiceSorteado = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[indiceSorteado];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Esse é o seu amigo secreto: <strong>${amigoSorteado}</strong></li>`;
+    console.log(amigoSorteado);
+}
